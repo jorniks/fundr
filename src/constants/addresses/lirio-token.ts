f9e8d51c080079777b2fac5d1f973a880e352859
jorniks <dictforum@gmail.com>
2024-07-03 18:30:19 +0100

    Added token address and abi

---
+++ src/constants/addresses/lirio-token.ts
@@ -0,0 +1,8 @@
+import { ChainId } from "@swapsicledex/swapsicle-sdk";
+
+export const LIRIO_TOKEN: { [chainId: number]: string } = {
+  [ChainId.BSC_TESTNET]: "0x6656d065a3744e97b018cc9a8caa50243a8546fc",
+  [ChainId.MANTLE_TESTNET]: "",
+  [ChainId.TELOS_TESTNET]: "",
+  [ChainId.MATIC_TESTNET]: "",
+}
