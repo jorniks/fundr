d0aacacdcfc6e7ead4851503990e58ec1bf9cbf3
jorniks <dictforum@gmail.com>
2024-07-03 20:40:21 +0100

    Updated contract address

--- src/constants/addresses/lirio-token.ts
+++ src/constants/addresses/lirio-token.ts
@@ -1,7 +1,7 @@
 import { ChainId } from "@swapsicledex/swapsicle-sdk";
 
 export const LIRIO_TOKEN: { [chainId: number]: string } = {
-  [ChainId.BSC_TESTNET]: "0x6656d065a3744e97b018cc9a8caa50243a8546fc",
+  [ChainId.BSC_TESTNET]: "0x6903cb12d9b815d4a50dde2051f148175d51af54",
   [ChainId.MANTLE_TESTNET]: "",
   [ChainId.TELOS_TESTNET]: "",
   [ChainId.MATIC_TESTNET]: "",
