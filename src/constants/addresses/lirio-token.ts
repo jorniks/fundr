a8d8fd378df6a7fce579ca2f97c7968d0209f3aa
jorniks <dictforum@gmail.com>
2024-07-10 16:22:02 +0100

    Update ABI and CA for telos token

--- src/constants/addresses/lirio-token.ts
+++ src/constants/addresses/lirio-token.ts
@@ -2,5 +2,5 @@ import { ChainId } from "@swapsicledex/swapsicle-sdk";
 
 export const LIRIO_TOKEN: { [chainId: number]: string } = {
   [ChainId.BSC_TESTNET]: "0x6903cb12d9b815d4a50dde2051f148175d51af54",
-  [ChainId.TELOS_TESTNET]: "0xe7Cbf74D15E282fd2b74F399dB1E28709B73C7aE",
+  [ChainId.TELOS_TESTNET]: "0xb6Bb36033d3b76De874Cd9386b877c12cBdC106E",
 }
