9ee029c86223c97963fdab17de92efbde0e2be1c
jorniks <dictforum@gmail.com>
2024-11-22 17:16:55 +0100

    Update: added how it works page

---
+++ src/app/how-it-works/page.tsx
@@ -0,0 +1,11 @@
+import HowItWorksComponent from '@/components/HowItWorks'
+
+const HowItWorksPage = () => {
+  return (
+    <main className="container py-40">
+      <HowItWorksComponent />
+    </main>
+  )
+}
+
+export default HowItWorksPage
