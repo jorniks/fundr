831b37d14f06c9429afac1a0c8f568ac47ca88ed
jorniks <dictforum@gmail.com>
2024-11-26 19:14:59 +0100

    Modified styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,65 +2,32 @@ import Image from 'next/image'
 
 const Footer = () => {
   return (
-    <footer className="mt-auto w-full max-w-[85rem] py-10 px-4 sm:px-6 lg:px-8 mx-auto border-t border-white/20">
-      <div className="text-center">
-        <a
-          className="flex justify-center items-center space-x-3 text-xl font-semibold text-gray-50 hover:text-lime-400 transition-colors duration-300 pb-5"
-          href="/"
-        >
-          <Image width={1000} height={1000} src="/fundr.png" alt="fundr." className="w-auto h-8" />
+    <footer className="container py-10 border-t border-white/20 text-center flex flex-col items-center gap-y-6">
+      <a href="/" className="text-gray-50 hover:text-lime-400 transition-colors duration-300">
+        <Image width={1000} height={1000} src="/fundr.png" alt="fundr." className="w-auto h-8" />
+      </a>
+
+      <div className="">
+        <p className="font-medium">Join the <a href="#" className="outline-none border-b border-dotted text-lime-500">fundr community</a> and help bring dreams to life.</p>
+        <article>&copy; fundr 2024, All rights reserved.</article>
+      </div>
+
+      <div className="space-x-6">
+        <a href="https://telegram.org" className="hover:text-lime-500 duration-200 outline-none" target="_blank" rel="noreferrer">
+          <i className="text-2xl bi bi-telegram"></i>
         </a>
 
-        <div className="mt-3">
-          <p className="text-gray-50">
-            Join the{" "}
-            <a
-              className="text-blue-600 decoration-2 hover:underline focus:outline-none focus:underline font-medium"
-              href="#"
-            >
-              Fundr Community
-            </a>{" "}
-            and help bring dreams to life.
-          </p>
-          <p className="text-gray-400">© Fundr 2024, All rights reserved.</p>
-        </div>
+        <a href="https://github.com/jorniks/fundr" className="hover:text-lime-500 duration-200 outline-none" target="_blank" rel="noreferrer">
+          <i className="text-2xl bi bi-github"></i>
+        </a>
 
-        <div className="mt-3 space-x-2">
-          <a
-            className="size-8 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-full border border-transparent text-gray-50 hover:bg-lime-500 ease-in-out duration-200 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none"
-            href="https://telegram.org"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="Fundr on Telegram"
-          >
-            <i className="text-xl bi bi-telegram"></i>
-          </a>
-          <a
-            className="size-8 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-full border border-transparent text-gray-50 hover:bg-lime-500 ease-in-out duration-200 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none"
-            href="https://github.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="Fundr on GitHub"
-          >
-            <i className="text-xl bi bi-github"></i>
-          </a>
-          <a
-            className="size-8 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-full border border-transparent text-gray-50 hover:bg-lime-500 ease-in-out duration-200 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none"
-            href="https://linkedin.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="Fundr on LinkedIn"
-          >
-            <i className="text-xl bi bi-linkedin"></i>
-          </a>
-          <a
-            className="size-8 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-full border border-transparent text-gray-50 hover:bg-lime-500 ease-in-out duration-200 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none"
-            href="mailto:contact@fundr.com"
-            aria-label="Contact Fundr via Email"
-          >
-            <i className="text-xl bi bi-envelope-fill"></i>
-          </a>
-        </div>
+        <a href="https://linkedin.com/in/johnokeke" className="hover:text-lime-500 duration-200 outline-none" target="_blank" rel="noreferrer">
+          <i className="text-2xl bi bi-linkedin"></i>
+        </a>
+
+        <a href="mailto:contact@fundr.com" className="hover:text-lime-500 duration-200 outline-none">
+          <i className="text-2xl bi bi-envelope-fill"></i>
+        </a>
       </div>
     </footer>
   )
