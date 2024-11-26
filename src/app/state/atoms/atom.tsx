897179035addd18f43dc059f2297a000ad0244a4
jorniks <dictforum@gmail.com>
2024-11-26 19:20:59 +0100

    Added new variables

--- src/app/state/atoms/atom.tsx
+++ src/app/state/atoms/atom.tsx
@@ -1,7 +1,23 @@
 "use client";
+import { preferredTokens } from "@/constants/addresses/preferred-tokens";
 import { atom } from "recoil";
 
 export const loadingState = atom({
   key: "loadingState",
   default: false, // Set your default loading state here
+});
+
+export const contributionAmount = atom({
+  key: "contributionAmount",
+  default: "", // Set your default amount state here
+});
+
+export const currentAllowance = atom({
+  key: "currentAllowance",
+  default: 0, // Set your default amount state here
+});
+
+export const tokenInfoObj = atom({
+  key: "tokenInfoObj",
+  default: preferredTokens[0], // Set your default amount state here
 });
