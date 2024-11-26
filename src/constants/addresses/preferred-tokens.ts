b5afaf14f423e44f99e65e9d1b1497aed149f876
jorniks <dictforum@gmail.com>
2024-11-26 08:38:09 +0100

    Update: used new contracts

--- src/constants/addresses/preferred-tokens.ts
+++ src/constants/addresses/preferred-tokens.ts
@@ -1,17 +1,12 @@
 export const preferredTokens = [
   {
     name: "USDT",
-    address: "0xF97186D89f49553CD33a7a72d8e5dDDD524BC23f",
-    decimal: 6
+    address: "0x2B2079C9B58534aFe02079b0C6D6B0DdAc5CF2d5",
+    decimal: 18
   },
   {
     name: "USDC",
-    address: "0xf17a935A89cB4CBc176cd5afeBCB9805e5Ed7c1A",
-    decimal: 6
-  },
-  {
-    name: "WETH",
-    address: "0xd6EEEBD93720CC75B5FAE70eA5430E1Fc4278Bf1",
+    address: "0x8853E66431951c90Fec5378cEE468b242D8DD92F",
     decimal: 18
   },
 ]
