bca1b3bb02f6e9509a38b59b1c9e33b7dc23f57e
jorniks <dictforum@gmail.com>
2024-11-29 13:53:51 +0100

    Update: new contract to fix claim issue and new token contracts

--- src/constants/addresses/preferred-tokens.ts
+++ src/constants/addresses/preferred-tokens.ts
@@ -1,12 +1,12 @@
 export const preferredTokens = [
   {
     name: "USDT",
-    address: "0x2B2079C9B58534aFe02079b0C6D6B0DdAc5CF2d5",
+    address: "0xeEf0Cf97bEd9336528Fa8F7fc64B5f0aBEEc7a26",
     decimal: 18
   },
   {
     name: "USDC",
-    address: "0x8853E66431951c90Fec5378cEE468b242D8DD92F",
+    address: "0xEf0618c04298b7cA9F4f09e86bE9153cD0DB9997",
     decimal: 18
   },
 ]
