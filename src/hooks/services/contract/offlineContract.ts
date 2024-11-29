66cca7e336ef137c6d05d21cd0900805f4e34f79
jorniks <dictforum@gmail.com>
2024-11-29 12:45:54 +0100

    Added more RPCs to combat failed network connection for offline loading

--- src/hooks/services/contract/offlineContract.ts
+++ src/hooks/services/contract/offlineContract.ts
@@ -1,9 +1,35 @@
 import RPC from "@/lib/rpc-list"
 import { defaultChainId } from "@/lib/services/chain-config"
 import { Contract, ethers, isAddress, JsonRpcSigner, Provider, ZeroAddress } from "ethers"
+import { useEffect, useState } from "react"
+
+const GetActiveProvider = (): any => {
+  const [activeURL, setActiveURL] = useState<string>("")
+
+  useEffect(() => {
+    retrieveActiveURL()
+  }, [])
+
+  const retrieveActiveURL = async () => {
+    for (const rpcURL of RPC[defaultChainId]) {
+      let provider = new ethers.JsonRpcProvider(rpcURL, undefined, { staticNetwork: ethers.Network.from(defaultChainId) });
+      
+      try {
+        await provider._detectNetwork();
+        setActiveURL(rpcURL);
+        break;
+      } catch (err) {
+      }
+    }
+  };
+
+  return activeURL
+}
 
 const getProviderOrSigner = (): JsonRpcSigner | Provider => {
-  return new ethers.JsonRpcProvider(RPC[defaultChainId], undefined, { staticNetwork: ethers.Network.from(defaultChainId) }) || ethers.getDefaultProvider(RPC[defaultChainId]);
+  const rpcURL = GetActiveProvider();
+  
+  return new ethers.JsonRpcProvider(rpcURL, undefined, { staticNetwork: ethers.Network.from(defaultChainId) }) || ethers.getDefaultProvider(rpcURL);
 }
 
 export const getOfflineContract = (contractAddress: string, contractABI: any): Contract => {
--- src/lib/rpc-list.ts
+++ src/lib/rpc-list.ts
@@ -1,6 +1,12 @@
 
 const RPC = {
-  59141: "https://rpc.sepolia.linea.build",
+  59141: [
+    "https://rpc.sepolia.linea.build",
+    "https://linea-sepolia-rpc.publicnode.com",
+    "https://linea-sepolia.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
+    "https://linea-sepolia.blockpi.network/v1/rpc/public",
+    "wss://linea-sepolia-rpc.publicnode.com",
+  ]
 }
 
 export default RPC
